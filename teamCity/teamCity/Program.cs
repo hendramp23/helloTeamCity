62674c364a729c73c9f63ba74aa3be7773f03b07
hendramp23 <iwayan.hendra.mp@gmail.com>
2016-02-18 15:55:47 +0800

    test team city

---
+++ teamCity/teamCity/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teamCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello team city is a simple code to test team city auto build");
+            Console.ReadKey();
+        }
+    }
+}
